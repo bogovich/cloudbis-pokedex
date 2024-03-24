f92a929f3b93587d446db624fe9f765c35509dcd
pkbogovic <pkbogovic@gmail.com>
2024-03-24 14:36:25 +0100

    Added initial/basic pokemon list view and pokemon view routes

---
+++ app/pokemon/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function PokemonView() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">Welcome to the details of a specific</h1>
+        </main>
+    );
+}
