0381153cfc57d0d96aac636c6b74650ea7fede3e
pkbogovic <pkbogovic@gmail.com>
2024-03-25 21:47:33 +0100

    Added sign in buttons to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { auth } from "@/auth";
+import SignInGoogle from '@/app/_components/SignInGoogle';
+import SignInGithub from '@/app/_components/SignInGithub';
+
+export default async function Home() {
+  let session = await auth();
+  let user = session?.user?.name;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-dvh flex-col items-center justify-center gap-4 p-12">
       <h1 className="text-4xl font-bold">Welcome to Pokedex</h1>
+
+
+      {user ? <p className="text-lg">Hello, {user}</p> : <>
+        <SignInGoogle />
+        <SignInGithub />
+      </>}
     </main>
   );
 }
