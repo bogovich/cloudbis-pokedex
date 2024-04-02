ef3cb43f80aa2569124e908e7ee38ef8e639984f
pkbogovic <pkbogovic@gmail.com>
2024-04-02 21:28:12 +0200

    Updated error, loading  and not found pages

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import BrowsePokemonButton from '@/app/_components/BrowsePokemonButton'
+
+export default function NotFound() {
+    return (
+        <main className="flex flex-grow flex-col items-center gap-4 lg:p-12 md:p-8 sm:p-6 p-4">
+            <h1 className="text-2xl font-bold">404 - Page not found</h1>
+            <Image src="/poke-not-found.webp" alt="Pokemon not found" width={600} height={600} className='rounded'/>
+            <p className="text-lg text-gray-600 dark:text-gray-300">Could not find requested page.</p>
+            <p className="text-lg text-gray-600 dark:text-gray-300">Please return to homepage or browse all Pokémon.</p>
+            <div className='flex items-center justify-center'>
+                <Link href="/" className="focus:outline-none text-sm px-5 py-2.5 mb-2 text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg mr-5">Return Home</Link>
+                <BrowsePokemonButton size="large" />
+            </div>
+        </main>
+    )
+}
--- app/pokemon/error.tsx
+++ app/pokemon/error.tsx
@@ -18,9 +18,9 @@ export default function Error({
         <main className="flex flex-grow flex-col items-center justify-center gap-4 lg:p-12 md:p-8 sm:p-6 p-4">
             <h1>Something went wrong!</h1>
             <p>
-                {error.message}
+                Error: {error.message}
             </p>
-            <button onClick={() => reset()}>
+            <button className="focus:outline-none text-sm px-5 py-2.5 mb-2 text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg mr-5" onClick={() => reset()}>
                 Try again
             </button>
         </main>
