b0f129454cfe77d70362880bdcb031d2a263a216
pkbogovic <pkbogovic@gmail.com>
2024-04-02 21:24:17 +0200

    Added size variant for browse pokemon button and added small variant to header

--- app/_components/BrowsePokemonButton.tsx
+++ app/_components/BrowsePokemonButton.tsx
@@ -2,14 +2,16 @@
 
 import Link from "next/link";
 
-const BrowsePokemonButton = () => {
+const BrowsePokemonButton = ({ size = 'large' }) => {
+  const sizeClasses = size === 'large' ? 'text-sm px-5 py-2.5 mb-2' : 'text-xs px-3 py-1.5';
+
   return (
     <Link
       href="/pokemon"
       type="button"
-      className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      className={`focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg ${sizeClasses} dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900`}
     >
-      Browse Pokémon
+      {size === 'large' ? "Browse Pokémon" : "Browse"}
     </Link>
   );
 };
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import ThemeSwitcher from '@/app/_components/ThemeSwitcher';
+import BrowsePokemonButton from '@/app/_components/BrowsePokemonButton';
 import { auth, signOut } from '@/auth';
 
 function SignOut() {
@@ -11,7 +12,7 @@ function SignOut() {
         await signOut();
       }}
     >
-      <button className="mr-5 hover:text-gray-900 dark:text-slate-300 dark:hover:text-slate-200" type="submit">Sign out</button>
+      <button className="text-gray-200 hover:text-gray-100 dark:text-slate-300 dark:hover:text-slate-200" type="submit">Sign out</button>
     </form>
   );
 }
@@ -20,13 +21,14 @@ const Header = async () => {
   const session = await auth();
 
   return (
-    <header className="text-gray-600 body-font border-b-2 dark:bg-gray-700">
+    <header className="text-gray-600 body-font border-b-2 bg-gray-500 dark:bg-gray-700">
       <div className="flex justify-between md:flex-row items-center py-3 lg:mx-12 md:mx-8 sm:mx-6 mx-4">
-        <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-0">
+        <Link href="/" className="flex title-font font-medium items-center text-slate-800 mb-0">
           <Image src="/Pokedex.svg" alt="Pokedex" width={35} height={35} />
-          <span className="ml-3 text-xl dark:text-slate-300">Pokédex</span>
+          <span className="sm:ml-3 text-base sm:text-xl text-gray-200 hover:text-gray-100 dark:text-slate-300 dark:hover:text-slate-200">Pokédex</span>
         </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center gap-2">
+          {session && <BrowsePokemonButton size="small"/>}
           {session && <SignOut />}
           <ThemeSwitcher />
         </nav>
