70507e1f2b337c254b13a566952d8ef32b394e38
pkbogovic <pkbogovic@gmail.com>
2024-04-03 10:19:09 +0200

    Removed unnecessary bottom and end margins

--- app/_components/RandomPokemonButton.tsx
+++ app/_components/RandomPokemonButton.tsx
@@ -14,7 +14,7 @@ const RandomPokemonButton = () => {
   return (
     <button
       onClick={() => router.push(`/pokemon/${randomId}`)}
-      className="focus:outline-none text-white bg-lime-700 hover:bg-lime-800 focus:ring-4 focus:bg-lime-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:bg-lime-900"
+      className="focus:outline-none text-white bg-lime-700 hover:bg-lime-800 focus:ring-4 focus:bg-lime-700 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:bg-lime-900"
     > Random Pokémon </button>
   );
 };
