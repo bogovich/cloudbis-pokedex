28ae32e414be1799c70755767db31dc100dec245
pkbogovic <pkbogovic@gmail.com>
2024-04-03 10:26:25 +0200

    Fix browse pokemon button margin

--- app/_components/BrowsePokemonButton.tsx
+++ app/_components/BrowsePokemonButton.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 
 const BrowsePokemonButton = ({ size = 'large' }) => {
-  const sizeClasses = size === 'large' ? 'text-sm px-5 py-2.5 mb-2' : 'text-xs px-3 py-1.5';
+  const sizeClasses = size === 'large' ? 'text-sm px-5 py-2.5' : 'text-xs px-3 py-1.5';
 
   return (
     <Link
